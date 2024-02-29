f829ab71e356631a4e79ab978090501b0ba0ade1
CAT <bigstirnabon@gmail.com>
2024-02-29 20:40:40 +0700

    TES

--- app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import {router as user} from "./api/user";
-export const app = express();
-
-app.use(bodyParser.text());
-app.use(bodyParser.json());
-
-app.use("/", user)
